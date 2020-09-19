d8ee027c7894aacfdb3a22ce05b990b34e294e76
Nicolas B <boullet.nicolas@gmail.com>
2020-09-19 12:13:55 +0200

    add component for header and welcome + mockups in png

---
+++ FRONT-Equiio/src/app/welcome/welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  styleUrls: ['./welcome.component.scss'],
+  templateUrl: './welcome.component.html',
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
