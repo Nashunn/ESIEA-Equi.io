4155929bc91c2feef15a4fd5b8cfa8077bd222cb
yohanleb <yohan.lebricquer@gmail.com>
2020-09-20 20:53:32 +0200

    Add first routes and user informations form

--- FRONT-Equiio/src/app/app-routing.module.ts
+++ FRONT-Equiio/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes  } from '@angular/router';
+import {UserComponent} from './user/user.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+];
 
 @NgModule({
   exports: [RouterModule],
--- FRONT-Equiio/src/app/app.module.ts
+++ FRONT-Equiio/src/app/app.module.ts
@@ -5,12 +5,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
-
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NbButtonModule, NbSidebarModule } from '@nebular/theme';
+
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbIconModule,
+  NbInputModule,
+  NbSidebarModule, NbTooltipModule,
+  NbTreeGridModule,
+} from '@nebular/theme';
 import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
 import { EquiioHeaderComponent } from './equiio-header/equiio-header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import {UserComponent} from "./user/user.component";
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -18,17 +27,24 @@ import { WelcomeComponent } from './welcome/welcome.component';
     AppComponent,
     EquiioHeaderComponent,
     WelcomeComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot({name: 'default'}),
     NbLayoutModule,
     NbEvaIconsModule,
+    NbIconModule,
     RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
     NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
     NbButtonModule,
+    NbTreeGridModule,
+    NbCardModule,
+    NbInputModule,
+    NbActionsModule,
+    NbTooltipModule,
   ],
   providers: [],
 })
