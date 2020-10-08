ae7f00c64a824a9d1a29ea048380590614f31d22
Nicolas B <boullet.nicolas@gmail.com>
2020-10-08 19:33:40 +0200

    add authent service

---
+++ FRONT-Equiio/src/app/services/authentication.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '@app/models';
+import { environment } from '@environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  // Getter for current user value
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  // Login
+  public login(
+    email: string,
+    password: string,
+  ) {
+    return this.http.post<any>(`${environment.apiUrl}/users/login`, { email, password })
+      .pipe(map((user) => {
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        return user;
+      }));
+  }
+
+  // Register
+  public register(
+    firstname: string,
+    lastname: string,
+    email: string,
+    tel: string,
+    licence: string,
+    password: string,
+    passwordConfirm: string,
+  ) {
+    return this.http.post<any>(`${environment.apiUrl}/users`, {
+      firstname,
+      lastname,
+      email,
+      tel,
+      licence,
+      password,
+      passwordConfirm,
+    })
+      .pipe(map((user) => {
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        return user;
+      }));
+  }
+
+  public logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
